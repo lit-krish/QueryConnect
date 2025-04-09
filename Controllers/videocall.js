f95e63d18c87c1340b9ad17603e5821df8d1fd4a
Krishna Agarwal <151874246+lit-krish@users.noreply.github.com>
2025-04-10 05:27:32 +0530

    26

--- Controllers/videocall.js
+++ Controllers/videocall.js
@@ -23,7 +23,7 @@ const videocall=(io)=>{
             console.log(roomid)
             const remote_socket_id=userid_to_socket.get(remoteid)
             console.log(remote_socket_id,"room id-",roomid)
-            socket.to(remote_socket_id).emit("answer-call",({offer,caller_socketid:socket.id,roomid}))
+            socket.to(remote_socket_id).emit("answer-call",{offer,caller_socketid:socket.id,roomid})
         })
 
         socket.on("call-accepted",({caller_socketid,answer})=>{
