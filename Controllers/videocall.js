84035bf44d0509d591c1799d62ad17759798ce7d
rialalwani <156527373+rialalwani@users.noreply.github.com>
2025-04-13 01:34:54 +0530

    handled negotiation

--- Controllers/videocall.js
+++ Controllers/videocall.js
@@ -23,6 +23,7 @@ const videocall=(io)=>{
             //console.log(roomid)
             const remote_socket_id=userid_to_socket.get(remoteid)
             //console.log(remote_socket_id,"room id-",roomid)
+            socket.emit("get-receiver-socket-id",remote_socket_id)
             socket.to(remote_socket_id).emit("answer-call",{offer,caller_socketid:socket.id,roomid})
         })
 
@@ -35,20 +36,14 @@ const videocall=(io)=>{
             socket.to(caller_socketid).emit("call-ended")
         })
 
-        socket.on("nego-needed",({offer,remoteid})=>{
-            console.log("nego needed" , remoteid)
-            const remote_socket_id=userid_to_socket.get(remoteid)
-            socket.to(remote_socket_id).emit("peer-nego-needed",({offer,caller_socketid:socket.id}))
-        })
-
-        socket.on("nego-needed2",({offer,caller_socketid})=>{
-            console.log("nego needed2" , caller_socketid)
-            socket.to(caller_socketid).emit("peer-nego-needed",({offer,reciver_socketid:socket.id}))
+        socket.on("nego-needed",({offer,remote_socketid})=>{
+            console.log("nego needed" , remote_socketid)
+            socket.to(remote_socketid).emit("peer-nego-needed",({offer,remote_socketid:socket.id}))
         })
         
-        socket.on("nego-final",({answer,caller_socketid})=>{
-            console.log("final nego",caller_socketid)
-            socket.to(caller_socketid).emit("nego-done",answer)
+        socket.on("nego-final",({answer,remote_socketid})=>{
+            console.log("final nego",remote_socketid)
+            socket.to(remote_socketid).emit("nego-done",answer)
         })
 
         socket.on("disconnect", () => {
